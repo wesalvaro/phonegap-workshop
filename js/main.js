b771c6cf7492a6f5548bd5ee8bb99e1fa5b9dd1b
Wes Alvaro <git@wesalvaro.com>
2014-04-30 17:42:42 +0000

    Added Alert

--- js/main.js
+++ js/main.js
@@ -5,16 +5,26 @@ var app = {
         this.store.findByName($('.search-key').val(), function(employees) {
             var l = employees.length;
             var e;
-            $('.employee-list').empty();
+            var elist = $('.employee-list').empty();
             for (var i=0; i<l; i++) {
                 e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+                elist.append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
             }
         });
     },
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
 
     initialize: function() {
-        this.store = new MemoryStore();
+        var self = this;
+        this.store = new MemoryStore(function() {
+            self.showAlert('Store initialized.', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
