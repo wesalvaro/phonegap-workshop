c27ed252e0c96bfb809089f00eb72b084bb136bc
Wes Alvaro <git@wesalvaro.com>
2014-04-30 17:45:44 +0000

    Change Alert button text

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ var app = {
     },
     showAlert: function (message, title) {
         if (navigator.notification) {
-            navigator.notification.alert(message, null, title, 'OK');
+            navigator.notification.alert(message, null, title, 'Sweet');
         } else {
             alert(title ? (title + ": " + message) : message);
         }
