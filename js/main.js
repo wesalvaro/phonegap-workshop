705ff0e66f621ea24db4d5deae89731f4e0ded99
Wes Alvaro <git@wesalvaro.com>
2014-04-30 22:45:26 +0000

    Update phonegang

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
-angular.module('emp', ['org.apache.cordova.dialogs']).
-  controller('employees', function(notification) {
+angular.module('emp', ['org.apache.cordova.dialogs', 'org.apache.cordova.geolocation']).
+  controller('employees', function(notification, $scope, geolocation) {
       var store = new MemoryStore(function() {
         notification.alert('Store initialized.', 'Info', 'Cool');
       });
       
       this.name = '';
       this.employees = [];
+      this.pos = geolocation.getCurrentPosition();
       
+      var self = this;
       this.search = function() {
-        var self = this;
         store.findByName(this.name, function(employees) {
           self.employees = employees;
         });
       };
+      
+      $scope.$on('position', function(pos) {
+        console.log('Position', pos);
+        self.pos = pos;
+      });
   });
 angular.bootstrap(angular.element('#app'), ['emp']);
